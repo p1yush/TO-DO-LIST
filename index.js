ebe88e0b5e405aa2430a2c62baec56bac9ccbf45
p1yush <rr2rinku@gmail.com>
2021-10-12 06:18:06 +0530

    todolist

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+let items = []
+let workItems = []
+
+app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    let day = new Date().toLocaleDateString("en-US", options)
+
+ res.render("list", {listTitle: day, newListItems: items})
+})
+
+app.post("/", function(req, res){
+    let item = req.body.newItem
+    if(req.body.list === "Work") {
+        workItems.push(item)
+        res.redirect("/work")
+    }
+    else {
+        items.push(item)
+        res.redirect("/")
+    }
+}) 
+
+app.get("/work", function(req,res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+
+app.listen(2000, function(){
+    console.log("Server started")
+})
